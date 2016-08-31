0f04bcbe8410b04605ab722c7771559aa1b7580c
Fangting <fangtingprahl@gmail.com>
2016-08-31 10:12:04 -0500

    add cost function

--- logisticR/logisticR.py
+++ logisticR/logisticR.py
@@ -19,4 +19,24 @@ def plot(X, Y):
     ax.scatter(X0[Y==1], X1[Y==1], marker='+', color='blue')
     ax.scatter(X0[Y==0], X1[Y==0], marker='o', color='red')
     plt.show()
-    plt.close()
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z));
+
+def costFunction(X, Y, **option):
+    m = Y.shape[0]
+    n = X.shape[1]
+
+    theta = option.get('theta')
+    if not theta:
+        theta = np.zeros((n + 1, 1))
+    XwithOnes = np.hstack((np.ones((m, 1)), X))
+    h = sigmoid(np.dot(XwithOnes, theta))
+
+    cost = (
+      - np.dot(np.transpose(Y), np.log(h))
+      - np.dot(np.transpose(1 - Y), np.log(1 - h))
+    ) / m
+
+    grad = np.dot(np.transpose(XwithOnes), (h - Y)) / m;
+    return (np.sum(cost), grad)
