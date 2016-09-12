7510f99256e7b8fa1853cf3ba3a87b07be934a47
Fangting <fangtingprahl@gmail.com>
2016-09-12 10:30:43 -0500

    minimize logistic regression cost function

--- logisticR/logisticR.py
+++ logisticR/logisticR.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from scipy import optimize
 
 from utils import utils;
 
@@ -25,13 +26,14 @@ def plot(X, Y):
 def sigmoid(z):
     return 1 / (1 + np.exp(-z));
 
-def costFunction(X, Y, **option):
+def cost(theta, X, Y):
     m = Y.shape[0]
     n = X.shape[1]
 
-    theta = option.get('theta')
-    if not theta:
+    if theta is None:
         theta = np.zeros((n + 1, 1))
+    else:
+        theta = np.reshape(theta, (n + 1, 1))
     XwithOnes = np.hstack((np.ones((m, 1)), X))
     h = sigmoid(np.dot(XwithOnes, theta))
 
@@ -40,5 +42,29 @@ def costFunction(X, Y, **option):
       - np.dot(np.transpose(1 - Y), np.log(1 - h))
     ) / m
 
-    grad = np.dot(np.transpose(XwithOnes), (h - Y)) / m;
-    return (np.sum(cost), grad)
+    return np.sum(cost)
+
+def grad(theta, X, Y):
+    m = Y.shape[0]
+    n = X.shape[1]
+
+    if theta is None:
+        theta = np.zeros((n + 1, 1))
+    else:
+        theta = np.reshape(theta, (n + 1, 1))
+
+    XwithOnes = np.hstack((np.ones((m, 1)), X))
+    h = sigmoid(np.dot(XwithOnes, theta))
+    grad = np.dot(np.transpose(XwithOnes), (h - Y)) / m
+    return np.ndarray.flatten(grad);
+
+def optimize_bfgs(X, Y, **option):
+    n = X.shape[1]
+    theta = np.zeros((n + 1, 1))
+    [theta_res, cost_min, grad_min] = optimize.fmin_bfgs(cost, theta, grad, args=(X, Y), disp=True)
+    plot_arg = option.get('plot')
+    if plot_arg:
+        plot(X, Y)
+        plt.plot()
+
+    return (theta_res, cost_min, grad_min)
